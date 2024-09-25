80117495c500d84f2450e4d8ece0f38b628e7377
KhuongNgo0310 <khuongngo0310@gmail.com>
2024-09-25 15:17:42 -0500

    First commit

---
+++ functions.js
@@ -0,0 +1,30 @@
+
+function hasLower(password){
+    for (let i in password){
+        if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122)
+            return true;
+    }
+    return false;
+}
+function hasUpper(password){
+    for (let i in password){
+        if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90)
+            return true;
+    }
+    return false;
+}
+function hasNumber(password){
+    for (let i in password){
+        if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57)
+            return true;
+    }
+    return false;
+}
+function entropy(password){
+    let range = 0;
+    range = hasLower(password) ? range + 24 : range;
+    range = hasUpper(password) ? range + 24 : range;
+    range = hasNumber(password) ? range + 10 : range;
+    return Math.log2(range)*(password.length);
+}
+console.log(entropy("ABCd11"));
